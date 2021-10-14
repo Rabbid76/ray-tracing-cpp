f9c7d572f8b99ce0e42d34b07d903f31d08b438b
Rabbid76 <Gernot.Steinegger@gmail.com>
2021-10-14 22:20:33 +0200

    ray tracing core unit test

--- ray-tracing-core/test/test_main.cpp
+++ ray-tracing-core/test/test_main.cpp
@@ -5,23 +5,31 @@
 #include "geometry/geometry_test.h"
 #include "material/material_test.h"
 #include "core/core_test.h"
+#include <string>
 
-void unit_test(void);
+void unit_test(std::string selected_test);
 
-int main(int, char**)
+int main(int noargs, char** args)
 {
-    unit_test();
+    const char* selected_test = noargs > 1 ? args[1] : "";
+    unit_test(selected_test);
     return 0;
 }
 
-void unit_test(void)
+void unit_test(std::string selected_test)
 {
     using namespace ray_tracing_core_unit_test;
 
-    math::math_unit_test();
-    pdf::probability_density_function_unit_test();
-    texture::texture_unit_test();
-    geometry::geometry_unit_test();
-    material::material_unit_test();
-    core::core_unit_tests();
+    if (selected_test.empty() || selected_test.starts_with("math"))
+        math::math_unit_test();
+    if (selected_test.empty() || selected_test.starts_with("pdf"))
+        pdf::probability_density_function_unit_test();
+    if (selected_test.empty() || selected_test.starts_with("texture"))
+        texture::texture_unit_test();
+    if (selected_test.empty() || selected_test.starts_with("geometry"))
+        geometry::geometry_unit_test();
+    if (selected_test.empty() || selected_test.starts_with("material"))
+        material::material_unit_test();
+    if (selected_test.empty() || selected_test.starts_with("core"))
+        core::core_unit_tests();
 }
